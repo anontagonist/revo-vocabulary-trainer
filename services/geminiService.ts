ec3fe77d07a47d0e1184ad2873eae01296512345
anontagonist <anontagonist@gmail.com>
2025-12-06 22:26:33 +0100

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -7,8 +7,7 @@ console.log("Mein Key ist:", import.meta.env.VITE_GEMINI_API_KEY); // Zum Testen
 const ai = new GoogleGenAI({
   apiKey: import.meta.env.VITE_GEMINI_API_KEY,
 });
-const ai = new GoogleGenAI({
-  apiKey: import.meta.env.VITE_GEMINI_API_KEY,
+const ai = new GoogleGenAI(import.meta.env.VITE_GEMINI_API_KEY),
 });
 
 const MODEL_NAME = 'gemini-2.5-flash';
